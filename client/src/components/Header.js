5eeff228188b1088ffdffae7c9dfa49ebfd35ba7
Laurie Gao <lauriegao123@gmail.com>
2020-02-17 21:44:15 -0500

    added billing

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
+import Stripe from './Stripe';
 
 class Header extends Component {
     renderContent() {
@@ -14,7 +15,13 @@ class Header extends Component {
                     </li>
                 );
             default:
-                return <li><a href="/api/logout">Logout</a></li>;
+                return [
+                    <li key="1"><Stripe /></li>,
+                    <li key="3" style={{ margin: '0 10px' }}>
+                        Credits: {this.props.auth.credits}
+                    </li>,
+                    <li key="2"><a href="/api/logout">Logout</a></li>
+                ];
         }
     }
 
--- models/User.js
+++ models/User.js
@@ -3,8 +3,8 @@ const { Schema } = mongoose;
 
 //Schema for mongoDB users collection
 const userSchema = new Schema({
-    googleId: String
-
+    googleId: String,
+    credits: { type: Number, default: 0 }
 });
 
 mongoose.model('users', userSchema);
