cf55fac67d87e8575eed56738e4601e4c81f522d
Laurie Gao <lauriegao123@gmail.com>
2020-02-18 23:48:29 -0500

    added sendgrid

---
+++ client/src/components/surveys/SurveyForm.js
@@ -0,0 +1,58 @@
+import _ from 'lodash';
+import React, { Component } from 'react';
+import { reduxForm, Field } from 'redux-form';
+import { Link } from 'react-router-dom';
+import SurveyField from './SurveyField';
+
+const FIELDS = [
+    { label: 'Survey Title', name: 'title'},
+    { label: 'Subject Line', name: 'subject'},
+    { label: 'Email Body', name: 'body'},
+    { label: 'Recipient List', name: 'emails'}
+]
+
+class SurveyForm extends Component {
+    renderFields() {
+        return _.map(FIELDS, ({ label, name }) => {
+            return (
+                    <Field 
+                        key={name}
+                        component={SurveyField} 
+                        type="text"
+                        label={label}
+                        name={name}
+                    />
+            )
+        })
+    }
+
+    render() {
+        return (
+            <div>
+                <form 
+                    onSubmit={this.props.handleSubmit(values => console.log(values))}
+                >
+                    {this.renderFields()}
+                    <Link to="/surveys" className="red btn-flat white-text">
+                        Cancel
+                    </Link>
+                    <button type="submit" className="teal btn-flat right white-text">
+                        Next
+                        <i className="material-icons right">done</i>
+                    </button>
+                </form>
+            </div>
+        );
+    }
+}
+
+function validate(values) {
+    const errors = {};
+
+    return errors;
+}
+
+export default reduxForm({
+    validate,
+    form: 'surveyForm'
+})(SurveyForm);
